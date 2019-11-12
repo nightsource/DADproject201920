bbc70ee0bdfc4730ef6fbd50c03ff309afb79672
Sofia Ilisa Santos <ilisa.santos@gmail.com>
2019-11-12 21:50:49 +0000

    Sofia first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+
+@section('title', 'Vue.js App')
+
+@section('content')
+
+<div class="jumbotron">
+    <h1>@{{title}}</h1>
+</div>
+npm
+
+
+@endsection
+@section('pagescript')
+
+@stop  
