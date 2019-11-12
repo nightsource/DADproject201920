a059960ec269bc03f7edc5266793d556b6ff5b04
RicardoBarbara <2162363@my.ipleiria.pt>
2019-11-12 22:02:24 +0000

    Finish authorizations

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', 'LoginControllerAPI@login');
+Route::post('login', 'LoginControllerAPI@login')->name('login');
 
 Route::middleware('auth:api')->post('logout', 'LoginControllerAPI@logout');
 
-/*Route::get('teste', function () {
+Route::middleware('auth:api')->get('teste', function () {
     return response()->json(['msg'=>'Só um teste'], 200);
-   });*/
+   });
 
