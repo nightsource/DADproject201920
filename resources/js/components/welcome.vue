da96e2962b2f76ff787b31760bcdb971502dab1d
Valter Ferreira <valter.rm.ferreira@gmail.com>
2019-11-13 13:25:52 +0000

    feature/US1

--- resources/js/components/welcome.vue
+++ resources/js/components/welcome.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>
-        <span>TEST MOFO</span>
+    <div class="logo">
+        <img src="storage\app\public\media\logo.png"></img>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data: function(){
+            return { 
+            }
+        },
+        methods: {
+            
+
+        },
 }
 </script>
 
 <style scoped>
-
+.logo {
+    text-align :center;
+    }
 </style>
