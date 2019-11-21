04c782e2ec77b7a12c14ed064a8d6592a2f5e8a9
RicardoBarbara <2162363@my.ipleiria.pt>
2019-11-21 14:12:59 +0000

    no message

--- resources/js/components/welcome.vue
+++ resources/js/components/welcome.vue
@@ -1,62 +1,125 @@
+<style>
+  .centered {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .centerStuff{
+    z-index: 2;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    position: relative;
+  }
+
+  h1 {
+      position: relative;
+      display: inline-block;
+      text-align: center;      
+      z-index: 1;
+      font-size: 65px;
+      color: #333333;
+      text-shadow: -1px 1px 3px rgba(0,0,0,0.7);
+  }
+
+  .head-link {
+    position: relative;
+    display: inline-block;
+    font-size: 25px;    
+    text-decoration: overline;
+    color: #333333;
+    -webkit-font-smoothing: antialiased;
+    text-shadow: -1px 1px 3px rgba(0,0,0,0.7);
+  }
+
+  .logo {    
+    max-width: 100px;
+    height: auto;
+    margin: auto;
+    padding-bottom: 3em;
+  }
+</style>
+
+
 <template>
-    <div>
-        <!--<div class="header">
-            <img class="logo" :src="'/media/logo.png'" >
-        </div>
-
-        <div>
-            <b-button>Button</b-button>
-            <b-button variant="danger">Button</b-button>
-            <b-button variant="success">Button</b-button>
-            <b-button variant="outline-primary">Button</b-button>
-        </div>-->
-
+  <div>
     <b-navbar toggleable="lg" type="dark" variant="dark">
-    <b-navbar-brand :to="'home'">NavBar</b-navbar-brand>
+        <b-navbar-brand class="w-25" :to="'home'">
+            <b-img class="w-50" :src="'/media/logo.png'"></b-img>
+            </b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
-      </b-navbar-nav>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
 
-      <!-- Right aligned nav items -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template v-slot:button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item v-if="!isLogged" :to="'profile'">Profile</b-dropdown-item>
-          <b-dropdown-item v-if="!isLogged" :to="'logout'">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+            <!-- logged -->
+            <b-navbar-nav v-if="isLogged">
+              <b-nav-item :to="'profile'">Profile</b-nav-item>
+              <b-nav-item-dropdown right v-if="!isLogged">
+                <template v-slot:button-content>
+                  <em>User</em>
+                </template>
+                <b-dropdown-item :to="'profile'" >Profile</b-dropdown-item>
+                <b-dropdown-item :to="'logout'">Sign Out</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
 
-        
-    </div>
+            <!-- not logged -->
+            <b-navbar-nav v-if="!isLogged">
+              <b-nav-item v-on:click.prevent="userWantsToLogin()">Login</b-nav-item>
+              <b-nav-item v-on:click.prevent="userWantsToRegister()">Register</b-nav-item>
+            </b-navbar-nav>
+
+          </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>-->
+    <!--<nav-bar
+      @user_Wants_To_Login="userWantsToLogin"
+      @user_Wants_To_Register="userWantsToRegister"></nav-bar>-->
+
+    <div class="centered">    
+      <div class="centerStuff" v-if="!user_wants_login && !user_wants_register">
+        <img class="logo" :src="'/favicon.ico'" > 
+        <h1>Virtual Wallet</h1>
+        <a class="head-link"> The only wallet you will ever need </a>   
+      </div>       
+
+      <user-login v-if="user_wants_login"></user-login>
+      <user-register v-if="user_wants_register"></user-register>
+    </div>    
+  </div>
 </template>
 
 <script>
+import LoginComponent from "./login";
+import RegisterComponent from "./user_components/register";
+import NavbarComponent from "./home/navbar";
+
+
 export default {
     data: function(){
             return {
-                
+              user_wants_login: false,
+              user_wants_register: false
             }
         },
-        methods: {
-            
-
-        },
+    methods: {   
+      userWantsToLogin() {        
+          this.user_wants_login=true;       
+          this.user_wants_register=false; 
+      },
+      userWantsToRegister() {
+          this.user_wants_login=false;       
+          this.user_wants_register=true;
+      },  
+    },
+  components: {
+    "user-login": LoginComponent,
+    "user-register": RegisterComponent,
+    "nav-bar": NavbarComponent
+  },
 }
 </script>
-
-<style scoped>
-.logo {
-    text-align :center;
-    width: 900px;
-    }
-</style>
