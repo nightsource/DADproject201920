21326b2a10681fcd4b1e09791a12291ff54c417d
Valter <valter.rm.ferreira@gmail.com>
2020-01-06 21:36:21 +0000

    reverse commit file

---
+++ resources/js/components/user_components/logout.vue
@@ -0,0 +1,26 @@
+<template>
+</template>
+
+<script type="text/javascript">    
+    export default {
+        data: function(){
+            return { }
+        },
+        methods: {
+            logout() {
+                console.log(this.user);
+                axios
+                    .post("api/logout")
+                    .then(response => {
+                        console.log(response);
+                        
+                        this.$root.deleteToken();
+                        this.$router.push( 'welcome' )
+                    });
+            }
+        },
+        mounted() {
+            this.logout();
+        }
+    }
+</script>
